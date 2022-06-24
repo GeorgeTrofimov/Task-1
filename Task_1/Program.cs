f169083d4a734a8a5580e01363e39d1e8412d35f
George <tro37295@gmail.com>
2022-06-25 00:20:57 +0500

    real task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите число A ");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numB = int.Parse(Console.ReadLine());
+
+if ((numA > numB))
+{
+    Console.WriteLine($"Число {numA} является максимальным ");
+}
+else
+{
+    Console.WriteLine($"Число {numB} является максиамльным");
+}
